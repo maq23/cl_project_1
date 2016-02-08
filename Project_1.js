4434f69ee31e3e6f5b2fab7df8c042d5692e6cba
maq23 <mmcconnell23@gmail.com>
2016-02-07 20:41:41 -0500

    add dropdown

--- Project_1.js
+++ Project_1.js
@@ -1,5 +1,30 @@
 /*Library Project JS*/
 
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
 /*FireBase DB*/
 
 <script src='https://cdn.firebase.com/js/client/2.2.1/firebase.js'></script>
